263b7fa533b7c0b2d4ee2a468cb232ad23bd8b24
Tami Sweitzer <tamara0715@gmail.com>
2025-02-15 23:55:11 -0600

    Correct route path in Event controller.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,12 +7,12 @@ use App\Models\Event;
 
 class EventController extends Controller {
   public function index() {
-    return view('events', ['events' => Event::all()]);
+    return view('events.index', ['events' => Event::all()]);
   }
 
   public function show($id) {
     $event = Event::find($id);
-    return view('event', ['event' => $event]);
+    return view('events.show', ['event' => $event]);
   }
 
   public function archivemonth() {
@@ -20,6 +20,6 @@ class EventController extends Controller {
     // $events = Event::all()->where();
 
     // For now:
-    return view('events-month', ['events' => Event::all(), 'year' => $year, 'mon' => $mon]);
+    return view('events.events-month', ['events' => Event::all(), 'year' => $year, 'mon' => $mon]);
   }
 }
