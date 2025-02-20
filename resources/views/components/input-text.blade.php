91a9e1dddbfb0fecec6757375a1b9542275c6e91
Tami Sweitzer <tamara0715@gmail.com>
2025-02-19 21:49:23 -0600

    Make components for text and textarea  inputs for forms.

--- resources/views/components/input-text.blade.php
+++ resources/views/components/input-text.blade.php
@@ -1,2 +1,1 @@
-<input type="text" {{ $attributes->merge(['class' => 'block w-full border shadow-inner p-1']) }} id="fullname"
-    class="" name="fullname">
+<input type="text" {{ $attributes->merge(['class' => 'block w-full border shadow-inner p-1']) }}>
