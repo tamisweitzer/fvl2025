63ed05c60b815d68b8e739984b7d4603ae5517dd
Tami Sweitzer <tamara0715@gmail.com>
2025-03-08 23:13:49 -0600

    Update index template for Venues to match Events.

--- resources/views/venues/index.blade.php
+++ resources/views/venues/index.blade.php
@@ -1,13 +1,21 @@
 <x-layout>
-    <x-slot:heading>Venues</x-slot:heading>
+    <x-wrapper-narrow class="mt-8 mb-8">
+        <x-page-title>All Live Music Venues</x-page-title>
 
-    <ul>
-        @foreach ($venues as $venue)
-            <li class="p-4 mb-2 bg-slate-50 border b-slate-100">
-                <a href="/cities/{{ $venue->id }}">
-                    {{ $venue['name'] }}
-                </a>
-            </li>
-        @endforeach
-    </ul>
+        <section class="mb-8">
+            @foreach ($venues as $venue)
+                <div
+                    class="p-2 bg-slate-50 border border-slate-200 hover:border hover:border-b-slate-300 hover:shadow-lg">
+                    <a href="/venues/{{ $venue->id }}" class="block">
+                        <span class=" text-gray-700 text-lg font-semibold">
+                            {{ $venue->name }}
+                        </span>
+                        <br>
+                        <span class="text-sm">{{ $venue->city->name }}, {{ $venue->state->name }}</span>
+
+                    </a>
+                </div>
+            @endforeach
+        </section>
+    </x-wrapper-narrow>
 </x-layout>
