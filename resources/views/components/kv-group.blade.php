c9398d9dc8198a9a5d45bc6f8615a5db3a11b10f
Tami Sweitzer <tamara0715@gmail.com>
2025-03-26 00:52:40 -0500

    Wrap key-value elements in a wrapping div, make it a component. Working on styling event details on the single event page, using flexbox.

---
+++ resources/views/components/kv-group.blade.php
@@ -0,0 +1,1 @@
+<div {{ $attributes->merge(['class' => 'flex gap-4 mb-2']) }}>{{ $slot }}</div>
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -2,33 +2,72 @@
     <x-wrapper-narrow class="mt-8 mb-8">
         <x-page-title>{{ $event->name }}</x-page-title>
 
-        <section class="mb-8">
-            <div>
-                <x-kv-key>Band:</x-kv-key>
+        <section data-event-details>
+            <x-kv-group>
+                <x-kv-key class="basis-20 text-right">Band:</x-kv-key>
                 <x-kv-value>{{ $event->band->fullname }}</x-kv-value>
-            </div>
-            <div>
-                <x-kv-key>Venue:</x-kv-key>
+            </x-kv-group>
+
+            <x-kv-group>
+                <x-kv-key class="basis-20 text-right">Venue:</x-kv-key>
                 <x-kv-value>{{ $event->venue->name }}</x-kv-value>
-            </div>
-            <div>
-                <x-kv-key>Location:</x-kv-key>
+            </x-kv-group>
+
+            <x-kv-group>
+                <x-kv-key class="basis-20 text-right">Location:</x-kv-key>
                 <x-kv-value>
                     {{ $event->venue->city->name }},
                     {{ $event->venue->state->name }}
                 </x-kv-value>
-            </div>
-            <div>
-                <x-kv-key>Date:</x-kv-key>
+            </x-kv-group>
+
+            <x-kv-group>
+                <x-kv-key class="basis-20 text-right">Date:</x-kv-key>
                 <x-kv-value>{{ date('M d, Y', strtotime($event->event_date)) }}</x-kv-value>
-            </div>
-            <div>
-                <x-kv-key>Time:</x-kv-key>
+            </x-kv-group>
+
+            <x-kv-group>
+                <x-kv-key class="basis-20 text-right">Time:</x-kv-key>
                 <x-kv-value>{{ date('g:i a', strtotime($event->event_time)) }}</x-kv-value>
-            </div>
+            </x-kv-group>
+
+            <x-kv-group>
+                <x-kv-key class="basis-20 text-right">Details:</x-kv-key>
+                <x-kv-value class="">{{ $event->event_details }}</x-kv-value>
+            </x-kv-group>
+        </section>
+
+        <hr class="mt-12 mb-8 w-3/4 mx-auto">
+
+        <section data-event-address>
             <div>
-                <x-kv-key>Details:</x-kv-key>
-                <x-kv-value>{{ $event->event_details }}</x-kv-value>
+                <h3 class="text-xl bold mb-2">Address:</h3>
+                <address>
+                    @if ($event->venue->fullname)
+                        {{ $event->venue->fullname }}<br>
+                    @else
+                        {{ $event->venue->name }}
+                    @endif
+                    @if ($event->venue->address1)
+                        {{ $event->venue->address1 }}<br>
+                    @endif
+                    @if ($event->venue->address2)
+                        {{ $event->venue->address2 }}<br>
+                    @endif
+                    @if ($event->venue->city->name)
+                        {{ $event->venue->city->name }},
+                    @endif
+                    @if ($event->venue->state->name)
+                        {{ $event->venue->state->name }}
+                    @endif
+                    @if ($event->venue->zip)
+                        {{ $event->venue->zip }}<br>
+                    @endif
+                    @if ($event->venue->phone)
+                        Ph: <a href="tel:{{ $event->venue->phone }}"
+                            class="inline-block mt-2">{{ $event->venue->phone }}</a>
+                    @endif
+                </address>
             </div>
         </section>
     </x-wrapper-narrow>
