b2e91be3571f5c93275a5d8710a0203ef92ca422
Tami Sweitzer <tamara0715@gmail.com>
2025-04-03 21:41:38 -0500

    Update Venue show blade to match pattern of other templates.

--- resources/views/venues/show.blade.php
+++ resources/views/venues/show.blade.php
@@ -1,18 +1,47 @@
 <x-layout>
-    <x-slot:heading>Venue</x-slot:heading>
-    <h2 class="text-2xl">{{ $venue['name'] }}</h2>
+    <x-wrapper-narrow class="mt-8 mb-8">
+        <x-page-title class="">{{ $venue->fullname }}</x-page-title>
+        <div class="-mt-4 mb-12 text-center">{{ $venue->excerpt }}</div>
 
-    <p>{{ $venue['name'] }}</p>
-
-    <h3 class="mt-8 mb-4">All Bands Playing Here</h3>
-    <div>
-        TODO
-        {{-- @foreach ($bands as $band)
-            <div class="p-4 mb-2 bg-slate-50 border b-slate-100">
-                {{ $band['name'] }}
+        <section class="mb-8">
+            <div class="mb-4">
+                <x-kv-key>About:</x-kv-key>
+                <x-kv-value>{{ $venue->bio }}</x-kv-value>
             </div>
-        @endforeach --}}
-    </div>
+            <div class="mb-4">
+                <x-kv-key>City:</x-kv-key>
+                <x-kv-value>{{ $venue->city->name }}</x-kv-value>
+            </div>
+            <div class="mb-4">
+                <x-kv-key>Website:</x-kv-key>
+                <x-kv-value>{{ $venue->website }}</x-kv-value>
+            </div>
+        </section>
+        <hr>
+        <section class="mt-8 mb-8">
+            <h2 class="mb-4 text-xl sm:text-2xl md:text-3xl">Upcoming Shows</h2>
+            TODO
+            {{-- @foreach ($events as $event)
+                <x-card-wrapper-secondary>
+                    <a href="/events/{{ $event->id }}" class="block">
+                        <div class="text-lg font-semibold text-gray-700">
+                            {{ $event->admin_name }}
+                        </div>
 
+                        <div class="text-gray-700">
+                            {{ $event->venue->fullname }}
+                            <span> in </span>
+                            {{ $event->venue->city->name }}
+                        </div>
 
+                        <div class="text-gray-500 text-sm">
+                            {{ $event->event_date }}
+                            {{ $event->event_time }}
+                        </div>
+                    </a>
+                </x-card-wrapper-secondary>
+            @endforeach --}}
+        </section>
+
+    </x-wrapper-narrow>
 </x-layout>
