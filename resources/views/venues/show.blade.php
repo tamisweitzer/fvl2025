6410a2d297f76c73a99785b5504b067ba9fd3b1b
Tami Sweitzer <tamara0715@gmail.com>
2025-04-03 21:33:40 -0500

    Correct variable name in Venue show template.

--- resources/views/venues/show.blade.php
+++ resources/views/venues/show.blade.php
@@ -1,16 +1,17 @@
 <x-layout>
-    <x-slot:heading>City</x-slot:heading>
-    <h2 class="text-2xl">{{ $city['name'] }}</h2>
+    <x-slot:heading>Venue</x-slot:heading>
+    <h2 class="text-2xl">{{ $venue['name'] }}</h2>
 
-    <p>{{ $city['name'] }}</p>
+    <p>{{ $venue['name'] }}</p>
 
     <h3 class="mt-8 mb-4">All Bands Playing Here</h3>
     <div>
-        @foreach ($bands as $band)
+        TODO
+        {{-- @foreach ($bands as $band)
             <div class="p-4 mb-2 bg-slate-50 border b-slate-100">
                 {{ $band['name'] }}
             </div>
-        @endforeach
+        @endforeach --}}
     </div>
 
 
