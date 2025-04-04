8ae9860164b7ad7b0e227e2552661247b009d4ca
Tami Sweitzer <tamara0715@gmail.com>
2025-04-04 01:12:23 -0500

    Add neighborhood to each city card on the index page for Cities.

--- resources/views/cities/index.blade.php
+++ resources/views/cities/index.blade.php
@@ -1,12 +1,16 @@
 <x-layout>
     <x-wrapper-narrow class="mt-8 mb-8">
-        <x-page-title>Cities</x-page-title>
+        <x-page-title>All Cities</x-page-title>
 
         <section class="mb-8">
             @foreach ($cities as $city)
                 <x-card-wrapper>
                     <a href="/cities/{{ $city->id }}">
-                        {{ $city->name }}
+                        <div class="text-lg font-semibold text-gray-700">
+                            {{ $city->name }}
+                        </div>
+
+                        <div class="text-gray-500 text-sm">{{ $city->neighborhood }}</div>
                     </a>
                 </x-card-wrapper>
             @endforeach
