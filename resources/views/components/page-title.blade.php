8552251749bb2aceea6b3d2c838d246a163d47e9
Tami Sweitzer <tamara0715@gmail.com>
2025-04-26 17:32:20 -0500

    Make font bold in  page title component.

--- resources/views/components/page-title.blade.php
+++ resources/views/components/page-title.blade.php
@@ -1,1 +1,2 @@
-<h1 {{ $attributes->merge(['class' => ' text-2xl sm:text-3xl md:text-4xl text-center mb-8']) }}>{{ $slot }}</h1>
+<h1 {{ $attributes->merge(['class' => 'font-bold text-2xl sm:text-3xl md:text-4xl text-center mb-8']) }}>
+    {{ $slot }}</h1>
