5b5ef6ea2ec7946267e475334fbe998e5a73d08e
Tami Sweitzer <tamara0715@gmail.com>
2025-06-18 17:19:04 -0500

    Start on the show blade for summer events.

---
+++ resources/views/summer/events/show.blade.php
@@ -0,0 +1,68 @@
+<x-layout>
+    <x-wrapper-narrow class="mt-8 mb-8 mx-auto">
+        <x-page-title>Event</x-page-title>
+
+        <section class="mb-8 flex flex-wrap gap-2 mx-auto">
+
+            <x-card-wrapper class="w-full ">
+                <a href="/summer/events/{{ $event->id }}" class="">
+                    <div class="flex flex-row gap-x-4 sm:flex-col">
+                        <div class="flex flex-col justify-end">
+                            <div class="mb-2">
+                                <span class="inline-block w-28">Event name</span>
+                                <span class="font-bold text-2xl">{{ $event->event_name }}</span>
+                            </div>
+                            <div class="mb-2">
+                                <span class="inline-block w-28">Venue:</span>
+                                <span class="">{{ $event->venue }}</span>
+                            </div>
+                            <div class="mb-2">
+                                <span class="inline-block w-28">City: </span>
+                                <span>{{ $event->city }}</span>
+                            </div>
+                            <div class="mb-2">
+                                <span class="inline-block w-28">Band(s):</span>
+                                <span class="font-extrabold">{{ $event->band }}</span>
+                            </div>
+
+
+
+                            <div class="mb-2">
+                                <span class="inline-block w-28">Start Date:</span>
+                                <span>{{ $event->start_date->format('M d, Y') }}</span>
+                            </div>
+                            <div class="mb-2">
+                                @if ($event->end_date)
+                                    <span class="inline-block w-28">End date:</span>
+                                    <span>{{ $event->end_date->format('M d, Y') }}</span>
+                                @endif
+                            </div>
+                            <div class="mb-2">
+                                <span class="inline-block w-28">Time:</span>
+                                <span>{{ $event->start_time }}</span>
+                            </div>
+
+
+
+                            <div class="mb-2">
+                                <span class="inline-block w-28">URL: </span>
+                                <span>{{ $event->url }}</span>
+                            </div>
+                            <div class="mb-2">
+                                <span class="inline-block w-28">Notes: </span>
+                                <span>{{ $event->notes }}</span>
+                            </div>
+                            <div class="mb-2">
+                                <span class="inline-block w-28">Type: </span>
+                                <span>{{ $event->event_type }}</span>
+                            </div>
+
+                        </div>
+                    </div>
+                    {{-- <div class="text-gray-500 text-sm">{{ $event->excerpt }}</div> --}}
+                </a>
+            </x-card-wrapper>
+
+        </section>
+    </x-wrapper-narrow>
+</x-layout>
