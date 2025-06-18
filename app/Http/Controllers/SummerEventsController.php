62dc35e087f68a37dd1a5b0f9df0a1b9f4865604
Tami Sweitzer <tamara0715@gmail.com>
2025-06-18 00:13:28 -0500

    Write the show and create methods for the summer events controller.

--- app/Http/Controllers/SummerEventsController.php
+++ app/Http/Controllers/SummerEventsController.php
@@ -11,4 +11,13 @@ class SummerEventsController extends Controller {
         $events = SummerEvents::all();
         return view('summer.events.index', ['events' => $events]);
     }
+
+    public function show($id) {
+        $event = SummerEvents::find($id);
+        return view('summer.events.show', ['event' => $event]);
+    }
+
+    public function create() {
+        return view('summer.events.create', []);
+    }
 }
