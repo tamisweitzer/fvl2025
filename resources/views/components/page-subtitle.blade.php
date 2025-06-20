7738b0f6f3d43f33c5e7b23b4e616c95d4d51b39
Tami Sweitzer <tamara0715@gmail.com>
2025-06-19 23:18:03 -0500

    Remove the mb for page subtitle component so that I can control the margin on a per-page basis because trying to override it each time was not working.

--- resources/views/components/page-subtitle.blade.php
+++ resources/views/components/page-subtitle.blade.php
@@ -1,2 +1,2 @@
-<h2 {{ $attributes->merge(['class' => 'font-light text-xl sm:text-1xl md:text-2xl text-center mb-8']) }}>
+<h2 {{ $attributes->merge(['class' => 'font-light text-xl sm:text-1xl md:text-2xl text-center']) }}>
     {{ $slot }}</h2>
