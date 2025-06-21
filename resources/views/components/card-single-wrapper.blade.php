85a728d5b9b098432e71f5fbe5f7707e1af510f1
Tami Sweitzer <tamara0715@gmail.com>
2025-06-21 13:01:10 -0500

    Create a new component to handle the card of a single summer event in a Show template.

---
+++ resources/views/components/card-single-wrapper.blade.php
@@ -0,0 +1,3 @@
+ <div
+     {{ $attributes->merge(['class' => 'px-4 pt-8 pb-12 mb-2 bg-slate-50 border border-slate-200 rounded shadow hover:shadow-lg hover:border-slate-300']) }}>
+     {{ $slot }}</div>
