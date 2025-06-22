24115a7f93cc60bc3f0360229322af08e09255a9
Tami Sweitzer <tamara0715@gmail.com>
2025-06-22 17:06:08 -0500

    Add mt-8 and lessen mb-8 to mb-6.

--- resources/views/components/page-title.blade.php
+++ resources/views/components/page-title.blade.php
@@ -1,2 +1,2 @@
-<h1 {{ $attributes->merge(['class' => 'font-bold text-2xl sm:text-3xl md:text-4xl text-center mb-8']) }}>
+<h1 {{ $attributes->merge(['class' => 'font-bold text-2xl sm:text-3xl md:text-4xl text-center mb-6 mt-8']) }}>
     {{ $slot }}</h1>
