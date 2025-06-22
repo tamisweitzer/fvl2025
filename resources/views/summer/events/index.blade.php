477aadb0caf209ed55c41049f67feb9976633727
Tami Sweitzer <tamara0715@gmail.com>
2025-06-22 18:20:53 -0500

    Change x-axis margin for list of summer events to make it easier to scroll the page without hitting the event links when on mobile.

--- resources/views/summer/events/index.blade.php
+++ resources/views/summer/events/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-wrapper-narrow class="mt-8 mb-8 mx-auto">
+    <x-wrapper-narrow class="mt-8 mb-8 mr-4">
         <x-page-title>Summer Events</x-page-title>
         <p class="-mt-4 mb-12 text-center">The complete list of summer events around the Fox Valley this summer.</p>
         <p class="text-center mb-2">Click on any listing for more information.</p>
