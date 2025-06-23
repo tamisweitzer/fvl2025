b452a1b81f2d5174295611955bc6f104fc90e54c
Tami Sweitzer <tamara0715@gmail.com>
2025-06-22 20:16:03 -0500

    Give the container that holds the list of summer events a little bit of right margin on mobile so that the user can scroll the page easily.

--- resources/views/summer/events/index.blade.php
+++ resources/views/summer/events/index.blade.php
@@ -1,12 +1,12 @@
 <x-layout>
-    <x-wrapper-narrow class="mt-8 mb-8 mr-4">
+    <x-wrapper-narrow class="mt-8 mb-8 mx-auto">
         <x-page-title>Summer Events</x-page-title>
         <p class="-mt-4 mb-12 text-center">The complete list of summer events around the Fox Valley this summer.</p>
         <p class="text-center mb-2">Click on any listing for more information.</p>
-        <div class="p-4 sm:p-8 bg-slate-50 border border-slate-300 shadow rounded">
+        <div class="mr-8 sm:mr-0 bg-slate-50 border border-slate-300 shadow rounded">
             @foreach ($events as $event)
-                <a href="/summer/events/{{ $event->id }}" class="block">
-                    <div class="my-4">
+                <a href="/summer/events/{{ $event->id }}" class="block ">
+                    <div class="px-4 py-4 hover:bg-orange-50">
                         <div class="font-bold">
                             {{ $event->band }}
                         </div>
