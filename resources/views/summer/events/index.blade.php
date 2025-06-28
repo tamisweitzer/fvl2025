193e7e5448cc6e3069a4a8a1d85a6e8ec2be3006
Tami Sweitzer <tamara0715@gmail.com>
2025-06-28 15:25:21 -0500

    Clean up only. Remove comments from summer events index page.

--- resources/views/summer/events/index.blade.php
+++ resources/views/summer/events/index.blade.php
@@ -5,7 +5,6 @@
         <p class="-mt-4 mb-12 text-center">The complete list of summer events around the Fox Valley this summer.</p>
         <p class="text-center mb-4 text-gray-600 italic text-sm">Click on any listing for more information.</p>
 
-        <!-- START TEST display by date -->
         @foreach ($events as $days)
             {{ $days[0]->start_date->format('D M d, Y') }}
             @foreach ($days as $event)
@@ -37,35 +36,5 @@
             @endforeach
             <hr class="mb-8">
         @endforeach
-        <!-- END TEST display by date -->
-        {{-- <div class="bg-slate-50 border border-slate-300 shadow rounded">
-            @foreach ($events as $event)
-                <a href="/summer/events/{{ $event->id }}" class="block ">
-                    <div class="px-4 py-4 hover:bg-orange-50">
-                        <div class="font-bold">
-                            {{ $event->band }}
-                        </div>
-                        <div>
-                            @if ($event->event_name)
-                                {{ $event->event_name }}
-                            @else
-                                {{ $event->venue }}
-                            @endif
-                        </div>
-                        <div>
-                            @if ($event->start_date)
-                                {{ $event->start_date->format('M d, Y') }}
-                            @endif
-                            @if ($event->start_time)
-                                - {{ $event->start_time }}
-                            @endif
-                        </div>
-                    </div>
-                    @if (!$loop->last)
-                        <hr>
-                    @endif
-                </a>
-            @endforeach
-        </div> --}}
     </x-wrapper-narrow>
 </x-layout>
