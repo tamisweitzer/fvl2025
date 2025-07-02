e1228c5713a027c5930111c27f51a11eb2ff625c
Tami Sweitzer <tamara0715@gmail.com>
2025-07-01 22:58:12 -0500

    Update the colors on the cancel and submit buttons to softer colors. The previous colors felt too aggressive.

--- resources/views/components/form-button-cancel.blade.php
+++ resources/views/components/form-button-cancel.blade.php
@@ -1,3 +1,3 @@
 <button
-    {{ $attributes->merge(['class' => 'btn mt-4 inline-block bg-red-500 hover:bg-red-600 active:bg-red-700 rounded px-4 py-2 text-white']) }}
+    {{ $attributes->merge(['class' => 'btn mt-4 inline-block bg-red-200 border border-red-300 text-red-700 hover:bg-red-300 active:bg-red-300 rounded px-4 py-2 text-white']) }}
     type="button">{{ $slot }}</button>
