c460e5597395f17ab82ba95eb484c69be3e5d756
Tami Sweitzer <tamara0715@gmail.com>
2025-07-03 01:00:48 -0500

    Remove the white text from the Delete and Submit buttons. They were correct in dev but not in prod.

--- resources/views/components/form-button-submit.blade.php
+++ resources/views/components/form-button-submit.blade.php
@@ -1,2 +1,2 @@
 <button type="submit"
-    {{ $attributes->merge(['class' => 'btn mt-4 inline-block bg-green-200 border border-green-400 text-green-700 hover:bg-green-300 active:bg-green-300 rounded px-4 py-2 text-white']) }}>{{ $slot }}</button>
+    {{ $attributes->merge(['class' => 'btn mt-4 inline-block bg-green-200 border border-green-400 text-green-700 hover:bg-green-300 active:bg-green-300 rounded px-4 py-2']) }}>{{ $slot }}</button>
