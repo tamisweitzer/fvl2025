43d3e14b48f791d58b9844ad5e745f424aea23c2
Tami Sweitzer <tamara0715@gmail.com>
2025-07-06 23:57:29 -0500

    Styling of the 'see more..' buttons. Lighter background, text dk orange insted of gray. Lighter border.

--- resources/views/components/see-more-events.blade.php
+++ resources/views/components/see-more-events.blade.php
@@ -3,47 +3,47 @@
 <div {{ $attributes->merge(['class' => 'mt-12']) }}>
     <div>
         <a href="/summer/events"
-            class="mb-4 mr-3 bg-orange-100 border border-orange-300 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
+            class="mb-4 mr-3 bg-orange-50 border border-orange-200 text-orange-700 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
             title="View the complete list of live bands playing in the Fox Cities of Wisconsin.">
             See <em>all</em> live music
         </a>
         <a href="/summer/events/bands"
-            class="mb-4 mr-3 bg-orange-100 border border-orange-300 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
+            class="mb-4 mr-3 bg-orange-50 border border-orange-200 text-orange-700 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
             title="Find out where your favorite band is playing this week.">
             Search by band
         </a>
         <a href="/summer/events/cities"
-            class="mb-4 mr-3 bg-orange-100 border border-orange-300 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
+            class="mb-4 mr-3 bg-orange-50 border border-orange-200 text-orange-700 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
             title="Find live music in your city">
             Search by city
         </a>
         <a href="/summer/events/venues"
-            class="mb-4 mr-3 bg-orange-100 border border-orange-300 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
+            class="mb-4 mr-3 bg-orange-50 border border-orange-200 text-orange-700 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
             title="Find live music playing at your favorite bar.">
             Search by venue
         </a>
         <a href="/summer/events/event-names"
-            class="mb-4 mr-3 bg-orange-100 border border-orange-300 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
+            class="mb-4 mr-3 bg-orange-50 border border-orange-200 text-orange-700 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
             title="Find live music at a particular event, such as Paperfest.">
             Search by event name
         </a>
         <a href="/summer/events/lunchtime-concerts"
-            class="mb-4 mr-3 bg-orange-100 border border-orange-300 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
+            class="mb-4 mr-3 bg-orange-50 border border-orange-200 text-orange-700 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
             title="Filter for live bands playing lunch time concerts.">
             Sort by lunch time concerts
         </a>
         <a href="/summer/events/bars-restaurants"
-            class="mb-4 mr-3 bg-orange-100 border border-orange-300 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
+            class="mb-4 mr-3 bg-orange-50 border border-orange-200 text-orange-700 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
             title="Filter for live bands playing at bars or restaurants">
             Sort by bar gigs
         </a>
         <a href="/summer/events/fairs-fests"
-            class="mb-4 mr-3 bg-orange-100 border border-orange-300 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
+            class="mb-4 mr-3 bg-orange-50 border border-orange-200 text-orange-700 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
             title="Filter for live bands playing at fairs, fests, or outdoor concerts.">
             Sort by fairs and fests
         </a>
         <a href="/summer/events/national-bands"
-            class="mb-4 mr-3 bg-orange-100 border border-orange-300 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
+            class="mb-4 mr-3 bg-orange-50 border border-orange-200 text-orange-700 hover:bg-orange-200 px-3 py-2 rounded inline-block shadow-md"
             title="Filter for national recording artists touring Wisconsin.">
             Sort by national acts
         </a>
