a02ae4be1232ed815344cd5e0361e733bf78b54f
Tami Sweitzer <tamara0715@gmail.com>
2025-07-07 21:11:37 -0500

    Add the 'see more...' buttons to the start of the summer events index page because it is a really long list so many people will probably abandon the list before getting to the end.

--- resources/views/summer/events/index.blade.php
+++ resources/views/summer/events/index.blade.php
@@ -3,8 +3,9 @@
     <x-wrapper-narrow class="mt-8 mb-8 mx-auto">
         <x-page-title>Summer Events</x-page-title>
         <p class="-mt-4 mb-12 text-center">The complete list of summer events around the Fox Valley this summer.</p>
-        <p class="text-center mb-4 text-gray-600 italic text-sm">Click on any listing for more information.</p>
-
+        <p class="text-center mb-4 text-gray-600 italic text-sm">Click on any listing for more information, or select a
+            button to filter results.</p>
+        <x-see-more-events class="mb-12"></x-see-more-events>
         @foreach ($events as $days)
             <div class="mt-4 border-b border-b-slate-500 text-orange-600 font-extrabold">
                 {{ $days[0]->start_date->format('D M d, Y') }}
