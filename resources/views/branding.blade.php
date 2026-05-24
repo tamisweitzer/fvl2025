86a093878bb3042dd2071914a25ecd83a198ba0f
Tami Sweitzer <tamara0715@gmail.com>
2026-05-24 00:01:11 -0500

    Start a branding document to keep track to site styles.

---
+++ resources/views/branding.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <div class="p-4 m-4">
+        <h1>Branding for Fox Valley Live v1.0_sqlite</h1>
+
+        <section class=my-8>
+            <h2>Surface colors</h2>
+            <div>
+                <div class="bg-gray-900 p-4 w-32 h-24"></div>
+                <p>bg-gray-900</p>
+            </div>
+        </section>
+    </div>
+
+</x-layout>
