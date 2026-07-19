1b392aa4558f58a32cc307488640bd723e14d66e
Tami Sweitzer <tamara0715@gmail.com>
2026-07-19 15:25:32 -0500

    Style summer events band list.

--- resources/views/summer/events/listbands.blade.php
+++ resources/views/summer/events/listbands.blade.php
@@ -1,15 +1,18 @@
 <x-layout>
+    <!-- summer/events/bands/ -->
     <x-wrapper-narrow class="mt-8 mb-8 mx-auto p-4">
         <x-page-title>Band list</x-page-title>
         <p class="-mt-4 mb-12 text-center">The complete list of the bands.</p>
-        <p class="text-center mb-4 text-gray-600 italic text-sm">Click on any listing for more information.</p>
+        <p class="text-center mb-4 italic text-sm">Click on any listing for more information.</p>
 
-        @foreach ($bands as $band)
-            <div class="mb-2">
-                <a href="/summer/events/bands/{{ $band->id }}"
-                    class="inline-block underline decoration decoration-orange-300 hover:bg-orange-100">{{ $band->band }}</a>
-            </div>
-        @endforeach
+        <div class="px-4 py-8 mx-2 sm:mx-4 bg-gray-800 rounded-lg overflow-y-auto">
+            @foreach ($bands as $band)
+                <div class="mb-4">
+                    <a href="/summer/events/bands/{{ $band->id }}">{{ $band->band }}</a>
+                </div>
+            @endforeach
+        </div>
+
 
         <x-see-more-events></x-see-more-events>
     </x-wrapper-narrow>
