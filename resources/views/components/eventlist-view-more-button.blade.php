3ab00324899829cd56243ec124b4afcbf4f38650
Tami Sweitzer <tamara0715@gmail.com>
2026-07-19 15:03:27 -0500

    Darken bg on the 'see more..' button from amber-500 to amber-600, and add an opacity of 85 to darken it a bit so that the style is more consistent with the headings and other elements.

--- resources/views/components/eventlist-view-more-button.blade.php
+++ resources/views/components/eventlist-view-more-button.blade.php
@@ -1,2 +1,3 @@
 <a
-    {{ $attributes->merge(['class' => 'm-4 mt-8 inline-block text-black font-bold rounded px-3 py-3 bg-amber-500 hover:bg-amber-600 active:bg-amber-700 shadow-lg']) }}>{{ $slot }}</a>
+    {{ $attributes->merge(['class' => 'm-4 mt-8 inline-block text-black font-bold rounded px-3 py-3 bg-amber-600 opacity-85 hover:bg-amber-600 active:bg-amber-700 shadow-lg']) }}>{{ $slot }}
+</a>
