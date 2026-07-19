166c96015a55ca60a17ca27c97c99ee8a36c4269
Tami Sweitzer <tamara0715@gmail.com>
2026-07-19 15:09:52 -0500

    Move the max width wrapper to outside the navbar component instead of inside. Move hero component outside of header component. Wrap main's slot component in a max width wrapper. Comment out content in footer component.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -42,62 +42,65 @@
 
 <body class="font-sans antialiased bg-gray-900 ">
     <header class="text-gray-300" style="box-shadow: 3px 5px 7px 7px rgba(0,0,0,0.3)">
-        @auth
-            <nav class=" bg-slate-900 p-0 text-sm">
-                <div class="secondary-nav-menu max-w-5xl mx-auto flex justify-end gap-2 p-0">
-                    <x-nav-link href="/summer/events/create" :active="request()->is('/summer/events/create/')">Add Summer Event</x-nav-link>
-                    <x-nav-link href="/bands/create" :active="request()->is('/bands/create/')">Add Band</x-nav-link>
-                    <x-nav-link href="/venues" :active="request()->is('/venues')">Add Venue</x-nav-link>
-                    <x-nav-link href="/events" :active="request()->is('events')">Add Event</x-nav-link>
-                </div>
-            </nav>
-        @endauth
-        <nav class="max-w-5xl mx-auto pr-4 flex justify-end gap-2 p-0 text-gray-200">
-            <a href="/"
-                class="site-branding inline-block  px-4 pt-2 pb-2 mr-auto text-orange-600 hover:text-amber-600 hover:underline hover:underline-offset-4 text-xl font-bold small-caps">
-                Fox Valley Live
-            </a>
+        <div class="max-w-4xl mx-auto">
+            @auth
+                <nav class=" bg-slate-900 p-0 text-sm">
+                    <div class="secondary-nav-menu   flex justify-end gap-2 p-0">
+                        <x-nav-link href="/summer/events/create" :active="request()->is('/summer/events/create/')">Add Summer Event</x-nav-link>
+                        <x-nav-link href="/bands/create" :active="request()->is('/bands/create/')">Add Band</x-nav-link>
+                        <x-nav-link href="/venues" :active="request()->is('/venues')">Add Venue</x-nav-link>
+                        <x-nav-link href="/events" :active="request()->is('events')">Add Event</x-nav-link>
+                    </div>
+                </nav>
+            @endauth
+            <nav class="max-w-5xl mx-auto pr-4 flex justify-end gap-2 p-0 text-gray-200">
+                <a href="/"
+                    class="site-branding inline-block  px-4 pt-2 pb-2 mr-auto text-orange-600 hover:text-amber-600 hover:underline hover:underline-offset-4 text-xl font-bold small-caps">
+                    Fox Valley Live
+                </a>
 
-            <div class="nav-menu mt-3">
-                <x-nav-link href="/summer/events" :active="request()->is('summer/events')">events</x-nav-link>
-                {{-- <x-nav-link href="/events" :active="request()->is('events')">events</x-nav-link> --}}
-                <x-nav-link href="/bands" :active="request()->is('bands')">bands</x-nav-link>
-                <x-nav-link href="/cities" :active="request()->is('cities')">cities</x-nav-link>
-                <x-nav-link href="/venues" :active="request()->is('venues')">venues</x-nav-link>
+                <div class="nav-menu mt-3">
+                    <x-nav-link href="/summer/events" :active="request()->is('summer/events')">events</x-nav-link>
+                    {{-- <x-nav-link href="/events" :active="request()->is('events')">events</x-nav-link> --}}
+                    <x-nav-link href="/bands" :active="request()->is('bands')">bands</x-nav-link>
+                    <x-nav-link href="/cities" :active="request()->is('cities')">cities</x-nav-link>
+                    <x-nav-link href="/venues" :active="request()->is('venues')">venues</x-nav-link>
 
-                @auth
+                    @auth
 
-                @endauth
-                {{-- <x-nav-link href="/summer/events/bands" :active="request()->is('bands')">bands</x-nav-link>
+                    @endauth
+                    {{-- <x-nav-link href="/summer/events/bands" :active="request()->is('bands')">bands</x-nav-link>
                 <x-nav-link href="/summer/events/cities" :active="request()->is('cities')">cities</x-nav-link>
                 <x-nav-link href="/summer/events/venues" :active="request()->is('venues')">venues</x-nav-link> --}}
 
-                <span class="inline-block mt-8 md:mt-0 md:ml-8">
-                    @guest
-                        {{-- <x-nav-link href="/login" :active="request()->is('login')">Log in</x-nav-link>
+                    <span class="inline-block mt-8 md:mt-0 md:ml-8">
+                        @guest
+                            {{-- <x-nav-link href="/login" :active="request()->is('login')">Log in</x-nav-link>
                         <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link> --}}
-                    @endguest
-                    @auth
-                        <form action="/logout" method="POST" class="inline">
-                            @csrf
-                            <input type="submit" value="Log out"
-                                class="btn inline text-inherit  hover:text-orange-600 active:text-orange-800 rounded px-4"></input>
-                        </form>
-                    @endauth
-                </span>
-            </div>
-            <div class="hamburger mt-3">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </div>
-        </nav>
+                        @endguest
+                        @auth
+                            <form action="/logout" method="POST" class="inline">
+                                @csrf
+                                <input type="submit" value="Log out"
+                                    class="btn inline text-inherit  hover:text-orange-600 active:text-orange-800 rounded px-4"></input>
+                            </form>
+                        @endauth
+                    </span>
+                </div>
+                <div class="hamburger mt-3">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </div>
+            </nav>
+        </div>
+    </header>
 
-        <!-- Hero image and welcome text.  -->
-        @if (request()->is('/'))
-            <div
-                class="p-8 bg-[url('../images/lita.jpg')] bg-cover bg-middle h-[300px] sm:h-[400px] md:h-[500px] lg:h-[500px]">
-                {{-- <div class="flex flex-col justify-center items-center align-middle max-w-6xl mx-auto mt-4  h-full">
+    <!-- Hero image and welcome text.  -->
+    @if (request()->is('/'))
+        <div
+            class="p-8 bg-[url('../images/lita.jpg')] bg-cover bg-middle h-[300px] sm:h-[400px] md:h-[500px] lg:h-[500px]">
+            {{-- <div class="flex flex-col justify-center items-center align-middle max-w-6xl mx-auto mt-4  h-full">
                     <div
                         class="content bg-white opacity-95 max-w-fit px-4 sm:px-10 pt-4 pb-6 font-extrabold text-center">
                         <p class="text-gray-800 font-semibold text-xl sm:text-3xl">The best live music in the Fox Valley
@@ -105,18 +108,17 @@
                         </p>
                     </div>
                 </div> --}}
-            </div>
-            <div class="px-4 py-1 text-right text-sm">Lita Ford, The Watering Hole, Green Bay</div>
-        @endif
-    </header>
+        </div>
+        <div class="px-4 py-1 text-right text-sm">Lita Ford, The Watering Hole, Green Bay</div>
+    @endif
 
     <main class="min-h-[calc(100vh-200px)] text-gray-300">
-        {{ $slot }}
+        <div class="max-w-4xl mx-auto">{{ $slot }}</div>
     </main>
 
     <footer class="mt-12 text-slate-400 py-16 text-center text-sm dark:text-white/70"
         style="box-shadow: -3px -5px 7px 7px rgba(0,0,0,0.3)">
-        <div class="wrapper max-w-5xl mx-auto ">
+        <div class="max-w-4xl mx-auto">
             <div class="flex flex-col sm:flex-row gap-4">
                 {{-- <div class="col flex-1">
                     <a href="#" class="block p-4">link</a>
@@ -127,21 +129,21 @@
 
                 {{-- <div class="mt-4 border-b border-b-slate-600 sm:hidden"></div> --}}
 
-                <div class="col flex-1">
+                {{-- <div class="col flex-1">
                     <a href="/bands" class="block p-4">bands</a>
                     <a href="/cities" class="block p-4">cities</a>
                     <a href="/events" class="block p-4">events</a>
                     <a href="/venues" class="block p-4">venues</a>
-                </div>
+                </div> --}}
 
                 {{-- <div class="mt-4 border-b border-b-slate-600 sm:hidden"></div> --}}
 
-                <div class="col flex-1">
+                {{-- <div class="col flex-1">
                     <a href="/about" class="block p-4">about</a>
                     <a href="/contact" class="block p-4">contact</a>
                     <a href="/" class="block p-4">home</a>
-                    {{-- <a href="#" class="block p-4">link</a> --}}
-                </div>
+
+                </div> --}}
             </div>
         </div>
     </footer>
