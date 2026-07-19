8dba3e0e8fc674fb0da599ca0b4dfa58796e8df7
Tami Sweitzer <tamara0715@gmail.com>
2026-07-19 16:57:01 -0500

    Add the shadow to the bottom of the hero, because the hero hides the shadow from the header. This makes it look like the hero is part of the header. Add top and bottom padding to the footer. Add copywrite to footer so that there is some content to see.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -98,28 +98,35 @@
 
     <!-- Hero image and welcome text.  -->
     @if (request()->is('/'))
-        <div
-            class="px-2 sm:px-4 bg-[url('../images/lita.jpg')] bg-cover bg-middle h-[300px] sm:h-[400px] md:h-[500px] lg:h-[500px]">
-            {{-- <div class="flex flex-col justify-center items-center align-middle max-w-6xl mx-auto mt-4  h-full">
-                    <div
-                        class="content bg-white opacity-95 max-w-fit px-4 sm:px-10 pt-4 pb-6 font-extrabold text-center">
-                        <p class="text-gray-800 font-semibold text-xl sm:text-3xl">The best live music in the Fox Valley
-                            <span class="text-orange-700 italic font-bold fvl-text-stroke"><br>...and beyond!</span>
-                        </p>
-                    </div>
-                </div> --}}
+        <div style="box-shadow: 3px 5px 7px 7px rgba(0,0,0,0.3)">
+
+            <div
+                class="px-2 sm:px-4 bg-[url('../images/lita.jpg')] bg-cover bg-middle h-[300px] sm:h-[400px] md:h-[500px] lg:h-[500px]">
+                {{-- <div class="flex flex-col justify-center items-center align-middle max-w-6xl mx-auto mt-4  h-full">
+                <div
+                class="content bg-white opacity-95 max-w-fit px-4 sm:px-10 pt-4 pb-6 font-extrabold text-center">
+                <p class="text-gray-800 font-semibold text-xl sm:text-3xl">The best live music in the Fox Valley
+                    <span class="text-orange-700 italic font-bold fvl-text-stroke"><br>...and beyond!</span>
+                </p>
+            </div>
+        </div> --}}
+            </div>
+            <div class="px-2 sm:px-4 py-2 text-gray-500 text-right text-xs sm:text-sm">Lita Ford, The Watering
+                Hole,
+                Green
+                Bay</div>
         </div>
-        <div class="px-4 py-1 text-right text-sm">Lita Ford, The Watering Hole, Green Bay</div>
     @endif
 
     <main class="min-h-[calc(100vh-200px)] text-gray-300">
         <div class="px-2 sm:px-4 max-w-4xl mx-auto">{{ $slot }}</div>
     </main>
 
-    <footer class="mt-12 px-4 text-slate-400 text-center text-sm dark:text-white/70"
+    <footer class="mt-12 px-4 py-8 text-slate-400 text-center text-sm dark:text-white/70"
         style="box-shadow: -3px -5px 7px 7px rgba(0,0,0,0.3)">
         <div class="px-2 sm:px-4 max-w-4xl mx-auto">
             <div class="flex flex-col sm:flex-row gap-4">
+                &copy; Fox Valley Live
                 {{-- <div class="col flex-1">
                     <a href="#" class="block p-4">link</a>
                     <a href="#" class="block p-4">link</a>
