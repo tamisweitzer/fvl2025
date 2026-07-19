35646b8ba4249ddd6fe6d3ad9005cbbffde1d6fb
Tami Sweitzer <tamara0715@gmail.com>
2026-07-19 15:31:58 -0500

    Changing show blade.

--- resources/views/summer/events/showband.blade.php
+++ resources/views/summer/events/showband.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
+    <!-- /summer/events/bands/{id} -->
     <x-wrapper-narrow class="mt-8 mb-8 mx-auto p-4">
         {{-- @dd($events) --}}
-        show band
         <x-page-title>
             @if (count($events) > 0)
                 {{ $events->first()->first()->band }}
@@ -9,41 +9,25 @@
                 generic title
             @endif
         </x-page-title>
-        <p class="-mt-4 mb-12 text-center">The complete list of events for
+        <p class="mb-12 text-center">The complete list of events for {{ $events->first()->first()->band }}
             this
             summer.</p>
-        <p class="text-center mb-4 text-gray-600 italic text-sm">Click on any listing for more information.</p>
+        <p class="text-center mb-4 italic text-sm">Click on any listing for more information.</p>
 
-        @foreach ($events as $days)
-            <div class="mt-4 border-b border-b-slate-500 text-orange-600 font-extrabold">
-                {{ $days[0]->start_date->format('D M d, Y') }}
-            </div>
-            @foreach ($days as $event)
-                <a href="/summer/events/{{ $event->id }}"
-                    class="block px-4 py-4 border-t border-b border-t-transparent border-b-transparent hover:bg-orange-50 hover:border-t hover:border-t-orange-300 hover:border-b hover:border-b-orange-300"
-                    title="Live music in {{ $event->city }} from {{ $event->band }} at {{ $event->venue }} @if ($event->event_name) for {{ $event->event_name }} @endif">
-                    <div class="font-bold">
-                        {{ $event->band }}
-                    </div>
-                    @if ($event->event_name)
-                        <div class="text-gray-800 text-sm">
-                            {{ $event->event_name }}
-                        </div>
-                    @endif
-                    <div class="text-gray-800 text-sm">
-                        {{ $event->venue }}
-                    </div>
-                    <div class="text-gray-800 text-sm">
-                        {{ $event->city }}
-                    </div>
-                    @if ($event->start_time)
-                        <div class="text-gray-800 text-sm">
-                            {{ $event->start_time }}
-                        </div>
-                    @endif
-                </a>
-            @endforeach
-        @endforeach
+        <div class="pb-8 mx-2 sm:mx-4 max-h- 96 bg-gray-800 rounded-lg overflow-y-auto">
+            @if (is_countable($events) && count($events) > 0)
+                @foreach ($events as $days)
+                    <x-eventlist-item-header>{{ $days[0]->start_date->format('D M d, Y') }}</x-eventlist-item-header>
+
+                    @foreach ($days as $event)
+                        <x-eventlist-item :event=$event href="/summer/events/{{ $event->id }}"
+                            class="mb-4"></x-eventlist-item>
+                    @endforeach
+                @endforeach
+            @endif
+
+        </div>
+
 
         <x-see-more-events></x-see-more-events>
     </x-wrapper-narrow>
