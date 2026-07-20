3cac42bd43fd2b0a69188c9076d2527550cdc909
Tami Sweitzer <tamara0715@gmail.com>
2026-07-19 20:44:35 -0500

    Correct template name for color swatches in the Branding component.

--- resources/views/branding/index.blade.php
+++ resources/views/branding/index.blade.php
@@ -1,26 +1,27 @@
 <x-layout>
     {{-- Site styles and component samples. --}}
+
+    <h1 class="text-3xl">Branding for Fox Valley Live v1.0_sqlite</h1>
+
     <div class="bg-white text-gray-900">
         <div class="p-4 m-4">
-            <h1 class="text-3xl">Branding for Fox Valley Live v1.0_sqlite</h1>
-
             <div class=my-8>
                 <h2>Surface colors</h2>
                 <div>
-                    <x-branding.color-swatch class="bg-gray-900"></x-brandingcolor-swatch>
-                        <p>bg-gray-900</p>
+                    <x-branding.color-swatch class="bg-gray-900"></x-branding.color-swatch>
+                    <p>bg-gray-900</p>
                 </div>
                 <div>
-                    <x-branding.color-swatch class="bg-gray-800"></x-brandingcolor-swatch>
-                        <p>bg-gray-800</p>
+                    <x-branding.color-swatch class="bg-gray-800"></x-branding.color-swatch>
+                    <p>bg-gray-800</p>
                 </div>
                 <div>
-                    <x-branding.color-swatch class="bg-gray-700"></x-brandingcolor-swatch>
-                        <p>bg-gray-700</p>
+                    <x-branding.color-swatch class="bg-gray-700"></x-branding.color-swatch>
+                    <p>bg-gray-700</p>
                 </div>
                 <div>
-                    <x-branding.color-swatch class="bg-gray-900"></x-brandingcolor-swatch>
-                        <p>bg-gray-900</p>
+                    <x-branding.color-swatch class="bg-gray-900"></x-branding.color-swatch>
+                    <p>bg-gray-900</p>
                 </div>
             </div>
 
@@ -28,7 +29,7 @@
         </div>
     </div>
 
-    <h2>Event List</h2>
+    <x-heading-secondary>Event List</x-heading-secondary>
     <div class="pb-8 mx-2 sm:mx-4 bg-gray-800 rounded-lg overflow-y-auto">
         @if (is_countable($eventsToday) && count($eventsToday) > 0) {{-- @dd($eventsToday) --}}
             @foreach ($eventsToday as $day)
