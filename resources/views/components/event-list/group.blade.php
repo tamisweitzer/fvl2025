0746ae89b9db93bef2bc2e7f896d327c7a961254
Tami Sweitzer <tamara0715@gmail.com>
2026-07-20 03:37:55 -0500

    Correct the component names for all event-list components.

--- resources/views/components/event-list/group.blade.php
+++ resources/views/components/event-list/group.blade.php
@@ -2,21 +2,21 @@
 
 @if (is_countable($events) && count($events) > 0)
     @foreach ($events as $day)
-        <x-event.list-item-header>{{ $day[0]->start_date->format('D M d, Y') }}</x-event.list-item-header>
+        <x-event-list.item-header>{{ $day[0]->start_date->format('D M d, Y') }}</x-event-list.item-header>
 
         @foreach ($day as $event)
-            <x-event.list-item :event=$event href="{{ $eventsUrl }}{{ $event->id }}"
-                class="mb-4"></x-event.list-item>
+            <x-event-list.item :event=$event href="{{ $eventsUrl }}{{ $event->id }}"
+                class="mb-4"></x-event-list.item>
         @endforeach
 
         @if (!$loop->last)
-            <x-event.list-item-divider></x-event.list-item-divider>
+            <x-event-list.item-divider></x-event-list.item-divider>
         @endif
     @endforeach
 
-    <x-event.list-view-more-button href="{{ $eventsUrl }}" title="{{ $viewMoreHoverText }}">
+    <x-event-list.view-more-button href="{{ $eventsUrl }}" title="{{ $viewMoreHoverText }}">
         {{ $viewMoreLinkText }}
-    </x-event.list-view-more-button>
+    </x-event-list.view-more-button>
 @else
     <div class="my-4 p-4 italic text-sm">
         {{ $noResultsText }}
