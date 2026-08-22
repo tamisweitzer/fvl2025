2fba6e84bfb36309c891dfae51017cbe0cbf2d9d
Tami Sweitzer <tamara0715@gmail.com>
2026-08-22 14:51:07 -0500

    Increase number of cites shown per row on city index page to make use of screen real estate.

--- resources/views/cities/index.blade.php
+++ resources/views/cities/index.blade.php
@@ -2,10 +2,10 @@
     <x-wrapper-narrow class="mt-8 mb-8">
         <x-page-title>All Cities</x-page-title>
 
-        <section class="mb-8">
+        <section class="mb-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2">
             @foreach ($cities as $city)
                 <x-card-wrapper>
-                    <a href="/cities/{{ $city->id }}">
+                    <a href="/cities/{{ $city->id }}" class="block">
                         <div class="mb-2 text-lg font-semibold text-gray-700">
                             {{ $city->name }}
                         </div>
