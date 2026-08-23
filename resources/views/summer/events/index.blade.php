ce1a46e1792ba5d9f2a4d86b60712187825a0263
Tami Sweitzer <tamara0715@gmail.com>
2026-08-23 17:11:43 -0500

    Remove the narrow wrapper from the summer events index page. This max width is being controlled in the main partial now.

--- resources/views/summer/events/index.blade.php
+++ resources/views/summer/events/index.blade.php
@@ -1,48 +1,45 @@
 <x-layout>
-    {{-- @dd($events); --}}
-    <x-wrapper-narrow class="mt-8 mb-8 mx-auto p-4">
-        <x-page-title>The Complete List of Live Music</x-page-title>
+    <x-page-title>The Complete List of Live Music</x-page-title>
 
-        <p class=" mb-12 text-center">The complete list of summer events around the Fox Valley this summer.</p>
+    <p class=" mb-12 text-center">The complete list of summer events around the Fox Valley this summer.</p>
 
-        <x-see-more-events-small class="mb-12"></x-see-more-events-small>
+    <x-see-more-events-small class="mb-12"></x-see-more-events-small>
 
-        <div>
-            <p class="text-center mb-4 mt-12  text-gray-400 italic text-sm">Click on any listing for more information, or
-                select a
-                button to filter results.</p>
-            @foreach ($events as $days)
-                <div class="mt-4 border-b border-b-slate-500 text-orange-600 font-extrabold">
-                    {{ $days[0]->start_date->format('D M d, Y') }}
-                </div>
-                @foreach ($days as $event)
-                    <a href="/summer/events/{{ $event->id }}"
-                        class="block px-4 py-4 border-t border-b border-t-transparent border-b-transparent hover:bg-gray-600"
-                        title="Live music in {{ $event->city }} from {{ $event->band }} at {{ $event->venue }} @if ($event->event_name) for {{ $event->event_name }} @endif">
-                        <div class="font-bold">
-                            {{ $event->band }}
-                        </div>
-                        @if ($event->event_name)
-                            <div class="text-sm">
-                                {{ $event->event_name }}
-                            </div>
-                        @endif
+    <div>
+        <p class="text-center mb-4 mt-12  text-gray-400 italic text-sm">Click on any listing for more information, or
+            select a
+            button to filter results.</p>
+        @foreach ($events as $days)
+            <div class="mt-4 border-b border-b-slate-500 text-orange-600 font-extrabold">
+                {{ $days[0]->start_date->format('D M d, Y') }}
+            </div>
+            @foreach ($days as $event)
+                <a href="/summer/events/{{ $event->id }}"
+                    class="block px-4 py-4 border-t border-b border-t-transparent border-b-transparent hover:bg-gray-600"
+                    title="Live music in {{ $event->city }} from {{ $event->band }} at {{ $event->venue }} @if ($event->event_name) for {{ $event->event_name }} @endif">
+                    <div class="font-bold">
+                        {{ $event->band }}
+                    </div>
+                    @if ($event->event_name)
                         <div class="text-sm">
-                            {{ $event->venue }}
+                            {{ $event->event_name }}
                         </div>
+                    @endif
+                    <div class="text-sm">
+                        {{ $event->venue }}
+                    </div>
+                    <div class="text-sm">
+                        {{ $event->city }}
+                    </div>
+                    @if ($event->start_time)
                         <div class="text-sm">
-                            {{ $event->city }}
+                            {{ $event->start_time }}
                         </div>
-                        @if ($event->start_time)
-                            <div class="text-sm">
-                                {{ $event->start_time }}
-                            </div>
-                        @endif
-                    </a>
-                @endforeach
-                {{-- <hr class="mb-8"> --}}
+                    @endif
+                </a>
             @endforeach
-            <x-see-more-events></x-see-more-events>
-        </div>
-    </x-wrapper-narrow>
+            {{-- <hr class="mb-8"> --}}
+        @endforeach
+        <x-see-more-events></x-see-more-events>
+    </div>
 </x-layout>
